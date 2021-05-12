86dbd3866244d68f446d8deeac4a7c967574b96c
Lucas Costa <lucas.costa4002@gmail.com>
2021-05-12 18:49:41 -0300

    Commit de todos os arquivos

---
+++ javascript.js
@@ -0,0 +1,143 @@
+const scrollSuave = (elem) => {
+    window.scroll({
+        top: document.querySelector(elem).offsetTop,
+        left: 0,
+        behavior: 'smooth'
+    })
+} 
+
+function abrirListaHci() {
+    document.querySelector('.medicos-conveniados-hci').style.display = 'block'
+}
+
+function fecharListaHci() {
+    document.querySelector('.medicos-conveniados-hci').style.display = 'none'
+}
+
+function abrirListaFisiolink() {
+    document.querySelector('.medicos-conveniados-fisiolink').style.display = 'block'
+}
+
+function fecharListaFisiolink() {
+    document.querySelector('.medicos-conveniados-fisiolink').style.display = 'none'
+}
+
+function abrirListaInstHernia() {
+    document.querySelector('.medicos-conveniados-instHernia').style.display = 'block'
+}
+
+function fecharListaInstHernia() {
+    document.querySelector('.medicos-conveniados-instHernia').style.display = 'none'
+}
+
+function abrirListaSulMin() {
+    document.querySelector('.medicos-conveniados-sulMineira').style.display = 'block'
+}
+
+function fecharListaSulMin() {
+    document.querySelector('.medicos-conveniados-sulMineira').style.display = 'none'
+}
+
+function abrirListaCItajuba() {
+    document.querySelector('.medicos-conveniados-cItajuba').style.display = 'block'
+}
+
+function fecharListaCItajuba() {
+    document.querySelector('.medicos-conveniados-cItajuba').style.display = 'none'
+}
+
+function abrirListaFisiocen() {
+    document.querySelector('.medicos-conveniados-fisiocenter').style.display = 'block'
+}
+
+function fecharListaFisiocen() {
+    document.querySelector('.medicos-conveniados-fisiocenter').style.display = 'none'
+}
+
+function abrirListaCeam() {
+    document.querySelector('.medicos-conveniados-ceam').style.display = 'block'
+}
+
+function fecharListaCeam() {
+    document.querySelector('.medicos-conveniados-ceam').style.display = 'none'
+}
+
+function abrirListaCiaSaude() {
+    document.querySelector('.medicos-conveniados-ciaDaSaude').style.display = 'block'
+}
+
+function fecharListaCiaSaude() {
+    document.querySelector('.medicos-conveniados-ciaDaSaude').style.display = 'none'
+}
+
+function abrirListaNcor() {
+    document.querySelector('.medicos-conveniados-ncor').style.display = 'block'
+}
+
+function fecharListaNcor() {
+    document.querySelector('.medicos-conveniados-ncor').style.display = 'none'
+}
+
+function abrirListaSoutoMaior() {
+    document.querySelector('.medicos-conveniados-soutoMaior').style.display = 'block'
+}
+
+function fecharListaSoutoMaior() {
+    document.querySelector('.medicos-conveniados-soutoMaior').style.display = 'none'
+}
+
+function abrirListaCooperciso() {
+    document.querySelector('.medicos-conveniados-cooperciso').style.display = 'block'
+}
+
+function fecharListaCooperciso() {
+    document.querySelector('.medicos-conveniados-cooperciso').style.display = 'none'
+}
+
+function abrirListaAtiva() {
+    document.querySelector('.medicos-conveniados-ativa').style.display = 'block'
+}
+
+function fecharListaAtiva() {
+    document.querySelector('.medicos-conveniados-ativa').style.display = 'none'
+}
+
+function abrirListaProDent() {
+    document.querySelector('.medicos-conveniados-prodent').style.display = 'block'
+}
+
+function fecharListaProDent() {
+    document.querySelector('.medicos-conveniados-prodent').style.display = 'none'
+}
+
+function abrirListaAline() {
+    document.querySelector('.medicos-conveniados-aline').style.display = 'block'
+}
+
+function fecharListaAline() {
+    document.querySelector('.medicos-conveniados-aline').style.display = 'none'
+}
+
+function abrirListaDonbay() {
+    document.querySelector('.medicos-conveniados-donbay').style.display = 'block'
+}
+
+function fecharListaDonbay() {
+    document.querySelector('.medicos-conveniados-donbay').style.display = 'none'
+}
+
+function abrirListaInnefro() {
+    document.querySelector('.medicos-conveniados-innefro').style.display = 'block'
+}
+
+function fecharListaInnefro() {
+    document.querySelector('.medicos-conveniados-innefro').style.display = 'none'
+}
+
+function abrirListaDonato() {
+    document.querySelector('.medicos-conveniados-donato').style.display = 'block'
+}
+
+function fecharListaDonato() {
+    document.querySelector('.medicos-conveniados-donato').style.display = 'none'
+}
